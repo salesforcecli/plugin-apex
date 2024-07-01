1481476787235914a10993794f6083a033bea94a
mshanemc <shane.mclaughlin@salesforce.com>
2024-07-01 17:01:46 -0500

    test: remove dynamic bin to drop oclif/core requirement, static run for UT

--- test/commands/apex/get/log.test.ts
+++ test/commands/apex/get/log.test.ts
@@ -5,11 +5,10 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 import fs from 'node:fs';
-import { Config } from '@oclif/core';
 import sinon from 'sinon';
 import { LogService } from '@salesforce/apex-node';
 import { expect } from 'chai';
-import { SfCommand } from '@salesforce/sf-plugins-core';
+import { stubSfCommandUx } from '@salesforce/sf-plugins-core';
 import { Org } from '@salesforce/core';
 import ansis from 'ansis';
 import Log, { LogGetResult } from '../../../../src/commands/apex/get/log.js';
@@ -18,14 +17,12 @@ const strip = new ansis.Ansis().strip;
 const logStripper = (log: LogGetResult[number]) => (typeof log === 'string' ? strip(log) : { log: strip(log.log) });
 
 describe('apex:log:get', () => {
-  let config: Config;
   let sandbox: sinon.SinonSandbox;
-  let logStub: sinon.SinonStub;
+  let uxStub: ReturnType<typeof stubSfCommandUx>;
 
   beforeEach(async () => {
-    config = await Config.load(import.meta.url);
     sandbox = sinon.createSandbox();
-    logStub = sandbox.stub(SfCommand.prototype, 'log');
+    uxStub = stubSfCommandUx(sandbox);
     sandbox.stub(Org, 'create').resolves({ getConnection: () => ({}) } as Org);
   });
 
@@ -42,38 +39,38 @@ describe('apex:log:get', () => {
 
   it('0 logs to get', async () => {
     sandbox.stub(LogService.prototype, 'getLogs').resolves([]);
-    const result = await Log.run([], config);
+    const result = await Log.run([]);
     expect(result).to.deep.equal([]);
-    expect(logStub.firstCall.args[0]).to.equal('No results found');
+    expect(uxStub.log.firstCall.args[0]).to.equal('No results found');
   });
 
   it('outputdir will write to fs', async () => {
     sandbox.stub(LogService.prototype, 'getLogs').resolves([{ log: 'myLog' }]);
-    const result = await new Log(['--output-dir', 'myDirectory'], config).run();
+    const result = await Log.run(['--output-dir', 'myDirectory']);
     expect(result).to.deep.equal(['myLog']);
-    expect(logStub.firstCall.args[0]).to.equal('Log files written to myDirectory');
+    expect(uxStub.log.firstCall.args[0]).to.equal('Log files written to myDirectory');
   });
 
   it('outputdir will write to fs --json', async () => {
     sandbox.stub(LogService.prototype, 'getLogs').resolves([{ log: 'myLog' }]);
-    const result = await new Log(['--outputdir', 'myDirectory', '--json'], config).run();
+    const result = await Log.run(['--outputdir', 'myDirectory', '--json']);
     expect(result).to.deep.equal(['myLog']);
-    expect(logStub.firstCall.args[0]).to.equal('Log files written to myDirectory');
+    expect(uxStub.log.firstCall.args[0]).to.equal('Log files written to myDirectory');
   });
 
   it('multiple results', async () => {
     sandbox.stub(LogService.prototype, 'getLogs').resolves([{ log: 'myLog' }, { log: 'myLog2' }]);
-    const result = (await new Log([], config).run()).map(logStripper);
+    const result = (await Log.run([])).map(logStripper);
     expect(result).to.deep.equal([{ log: 'myLog' }, { log: 'myLog2' }]);
-    expect(logStripper(logStub.firstCall.args[0] as string)).to.equal('myLog');
-    expect(logStripper(logStub.secondCall.args[0] as string)).to.equal('myLog2');
+    expect(logStripper(uxStub.log.firstCall.args[0] as string)).to.equal('myLog');
+    expect(logStripper(uxStub.log.secondCall.args[0] as string)).to.equal('myLog2');
   });
 
   it('multiple results --json', async () => {
     sandbox.stub(LogService.prototype, 'getLogs').resolves([{ log: 'myLog' }, { log: 'myLog2' }]);
-    const result = (await new Log(['--json'], config).run()).map(logStripper);
+    const result = (await Log.run(['--json'])).map(logStripper);
     expect(result).to.deep.equal([{ log: 'myLog' }, { log: 'myLog2' }]);
-    expect(logStripper(logStub.firstCall.args[0] as string)).to.equal('myLog');
-    expect(logStripper(logStub.secondCall.args[0] as string)).to.equal('myLog2');
+    expect(logStripper(uxStub.log.firstCall.args[0] as string)).to.equal('myLog');
+    expect(logStripper(uxStub.log.secondCall.args[0] as string)).to.equal('myLog2');
   });
 });
